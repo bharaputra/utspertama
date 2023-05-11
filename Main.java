36370c733fdc3f8371dd2a527c3bb694de0b35a8
bharaputra <bharaputra16@gmail.com>
2023-05-11 16:30:39 +0700

    uts

--- Main.java
+++ Main.java
@@ -1,5 +1,18 @@
+import pengguna.*;
+
 class Main {
     public static void main(String[] args) {
 
+            Mhs mhs = new Mhs(username:"ini_username", password:"ini_password");
+
+            mhs.setNama(nama:"Bhara Putra Agus Tiono");
+            mhs.setNim(nim:"G.111.19.0049");
+            mhs.setSmt(smt:8);
+            
+            System.out.println("Nama Mhs: " + mhs.getNama());
+            System.out.println("nim Mhs: " + mhs.getNim());
+            System.out.println("Smt Mhs: " + mhs.getSmt());
+            System.out.println("Username: " + mhs.getUsername());
+            System.out.println("Password Mhs: " + mhs.getPassword());
     }
 }
--- pengguna/Mhs.java
+++ pengguna/Mhs.java
@@ -1,7 +1,54 @@
 package pengguna;
 
-class Mhs {
-    Mhs () {
-testing
+public class Mhs {
+    private String username;
+    private String password;
+    private String nama;
+    private String nim;
+    private int smt;
+
+    public Mhs(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNama() {
+        return this.nama;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+
+    public String getNim() {
+        return this.nim;
+    }
+
+    public void setSmt(int smt) {
+        this.smt = smt;
+    }
+
+    public int getSmt() {
+        return this.smt;
     }
 }
